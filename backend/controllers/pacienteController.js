12b9097523b0e6a537e4bb4f066dd21331d0dfab
JPalacio <joelpalacio630@gmail.com>
2024-04-26 20:19:48 -0600

    feat : Backend - CRUD: Funcionalidad para el controlador de Actualizar un paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -21,16 +21,42 @@ const obtenerPaciente = async ( req, res ) => {
     const { id } = req.params;
     const paciente = await Paciente.findById( id );
 
+    if ( !paciente ) {
+        res.status( 404 ).json( { msg: 'NO ENCONTRADO' } );
+    }
     if ( paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
         return res.json( { msg: "Acción NO Valida" } );
     };
 
-    if ( paciente ) {
-        res.json( paciente );
-    }
+    res.json( paciente );
 };
 
-const actualizarPaciente = async ( req, res ) => { };
+const actualizarPaciente = async ( req, res ) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById( id );
+
+    if ( !paciente ) {
+        res.status( 404 ).json( { msg: 'NO ENCONTRADO' } );
+    }
+
+    if ( paciente.veterinario._id.toString() !== req.veterinario._id.toString() ) {
+        return res.json( { msg: "Acción NO Valida" } );
+    };
+
+    // Actualizar paciente
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json( pacienteActualizado );
+    } catch ( error ) {
+        console.log( error );
+    }
+};
 
 const eliminarPaciente = async ( req, res ) => { };
 
