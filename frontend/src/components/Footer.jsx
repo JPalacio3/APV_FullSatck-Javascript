cc2cadb818754e9f35e9c58063c12c960d6afc75
JPalacio <joelpalacio630@gmail.com>
2024-05-16 16:50:32 -0600

    feat : Frontend - Implementación del Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,14 @@
 
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <footer className="py-10">
+                <p className="text-center">
+                    <span className="text-indigo-600 font-bold">APV</span>  - Administrador de Pacientes de
+                    {' '}
+                    <span className="text-indigo-600 font-bold">Veterinaria</span>
+                </p>
+            </footer>
+        </>
     )
 }
